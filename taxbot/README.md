e0aa1cd2e2232f46d23fb712376457914e3e94bb
TJ Palanca <mail@tjpalanca.com>
2024-03-24 14:11:11 +0800

    Taxbot Working Example
    Basic working example using Playwright via RobotFramework
    
    Fixes #1

--- taxbot/README.md
+++ taxbot/README.md
@@ -1,10 +1,12 @@
 # TaxBot 
 
-## Setup instructions
+## Motivation
+
+Simple utility to reset the passwords in Chinabank's tax payment portal, which is required quarterly.
+
+## Instructions
 
 1. Run `poetry install` and use the poetry environment.
 2. Run `rfbrowser init` to set up requirements for robotframework
-
-## Reference
-
-* [RobotFramework Browser install instructions](https://github.com/MarketSquare/robotframework-browser#installation-instructions)
+3. Inject the secret references with the 1password CLI by running `cat config.tpl.yml | op inject >> config.yml`
+4. Run the script by running `python -m taxbot` in the poetry environment.
--- taxbot/taxbot/__main__.py
+++ taxbot/taxbot/__main__.py
@@ -1,9 +1,60 @@
-from RPA.Browser.Playwright import Playwright
+import Browser
+import yaml
+from Browser.utils.data_types import PageLoadStates, SelectionStrategy
 
-TAX_PAYMENT_PAGE = (
-    "https://www.bancnetonline.com/apps/servlet/ServletFront?trxcode=0011"
-)
+with open("config.yml", "r") as config_file:
+    credentials = yaml.safe_load(config_file)["credentials"]
 
-browser = Playwright()
-browser.open_browser(url=TAX_PAYMENT_PAGE)
-browser.fill_text("#loginid", txt="cbclicerdc")
+LOGIN_URL = "https://www.bancnetonline.com/apps/servlet/ServletFront?trxcode=0011"
+
+browser = Browser.Browser()
+browser.new_browser(headless=False)
+
+for user, cred in credentials.items():
+    # Open a new page to the tax portal
+    browser.new_page(LOGIN_URL)
+    browser.wait_for_load_state(PageLoadStates.networkidle)
+
+    # Login to the portal
+    browser.set_selector_prefix("frame[name='mainFrame'] >>>")
+    browser.fill_text("#loginid", txt=user)
+    browser.fill_text("#loginpass", txt=cred["old"]["password"])
+    browser.click("input[name='LOGIN']")
+    browser.wait_for_load_state(PageLoadStates.networkidle)
+
+    browser.fill_text("#answer", txt=cred["old"]["challenge"]["answer"])
+    browser.click("input[name='SUBMIT']")
+    browser.wait_for_load_state(PageLoadStates.networkidle)
+
+    # Go to change password screen
+    browser.set_selector_prefix("frame[name='leftFrame'] >>>")
+    browser.click(
+        browser.get_element_by(SelectionStrategy("Text"), text="Change Password")
+    )
+    browser.wait_for_load_state(PageLoadStates.networkidle)
+
+    # Submit new details
+    browser.set_selector_prefix("frame[name='mainFrame'] >>>")
+
+    browser.fill_text("#oldpas", txt=cred["old"]["password"])
+    browser.fill_text("#newpas", txt=cred["new"]["password"])
+    browser.fill_text("#cnfrmpwd", txt=cred["new"]["password"])
+
+    browser.fill_text("#question", txt=cred["old"]["challenge"]["question"])
+    browser.fill_text("#newquestion", txt=cred["new"]["challenge"]["question"])
+
+    browser.fill_text("#oldanswer", txt=cred["old"]["challenge"]["answer"])
+    browser.fill_text("#newanswer", txt=cred["new"]["challenge"]["answer"])
+    browser.fill_text("#cqconfirmans", txt=cred["new"]["challenge"]["answer"])
+
+    # browser.set_selector_prefix("frame[name='bottomFrame'] >>>")
+    # browser.click("input#btnOk")
+    browser.wait_for_load_state(PageLoadStates.networkidle)
+
+    # Logout
+    browser.set_selector_prefix("frame[name='leftFrame'] >>>")
+    browser.click(browser.get_element_by(SelectionStrategy("Text"), text="Logout"))
+    browser.wait_for_load_state(PageLoadStates.networkidle)
+
+    # Close page
+    browser.close_page()
