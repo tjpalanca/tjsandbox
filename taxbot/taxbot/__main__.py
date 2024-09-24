41e281b431d34fee400d9d910e55f9b1847d6b8d
TJ Palanca <mail@tjpalanca.com>
2024-09-24 11:24:20 +0800

    Add throttle seconds

--- taxbot/taxbot/__main__.py
+++ taxbot/taxbot/__main__.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import Browser
 import yaml
 from Browser.utils.data_types import PageLoadStates, SelectionStrategy
@@ -6,6 +8,7 @@ with open("config.yml", "r") as config_file:
     credentials = yaml.safe_load(config_file)["credentials"]
 
 LOGIN_URL = "https://www.bancnetonline.com/apps/servlet/ServletFront?trxcode=0011"
+THROTTLE_SECONDS = 3
 
 browser = Browser.Browser()
 browser.new_browser(headless=False)
@@ -13,6 +16,7 @@ browser.new_browser(headless=False)
 for user, cred in credentials.items():
     # Open a new page to the tax portal
     browser.new_page(LOGIN_URL)
+    sleep(THROTTLE_SECONDS)
     browser.wait_for_load_state(PageLoadStates.networkidle)
 
     # Login to the portal
@@ -20,10 +24,12 @@ for user, cred in credentials.items():
     browser.fill_text("#loginid", txt=user)
     browser.fill_text("#loginpass", txt=cred["old"]["password"])
     browser.click("input[name='LOGIN']")
+    sleep(THROTTLE_SECONDS)
     browser.wait_for_load_state(PageLoadStates.networkidle)
 
     browser.fill_text("#answer", txt=cred["old"]["challenge"]["answer"])
     browser.click("input[name='SUBMIT']")
+    sleep(THROTTLE_SECONDS)
     browser.wait_for_load_state(PageLoadStates.networkidle)
 
     # Go to change password screen
@@ -31,6 +37,7 @@ for user, cred in credentials.items():
     browser.click(
         browser.get_element_by(SelectionStrategy("Text"), text="Change Password")
     )
+    sleep(THROTTLE_SECONDS)
     browser.wait_for_load_state(PageLoadStates.networkidle)
 
     # Submit new details
@@ -49,12 +56,15 @@ for user, cred in credentials.items():
 
     browser.set_selector_prefix("frame[name='bottomFrame'] >>>")
     browser.click("input#btnOk")
+    sleep(THROTTLE_SECONDS)
     browser.wait_for_load_state(PageLoadStates.networkidle)
 
     # Logout
     browser.set_selector_prefix("frame[name='leftFrame'] >>>")
     browser.click(browser.get_element_by(SelectionStrategy("Text"), text="Logout"))
+    sleep(THROTTLE_SECONDS)
     browser.wait_for_load_state(PageLoadStates.networkidle)
 
     # Close page
     browser.close_page()
+    sleep(THROTTLE_SECONDS)
